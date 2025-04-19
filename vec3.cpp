65be850a7a97898946f079336d32a452343b56ec
GeraldJones <gajone12@asu.edu>
2025-04-18 18:53:24 -0700

    Do not need .cpp class, only header file and create the full 'vec3' class inline.

--- vec3.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Gerald Jones on 4/18/25.
-//
-
-#include "vec3.h"
--- vec3.h
+++ vec3.h
@@ -9,21 +9,11 @@
 #include <stdlib.h>
 #include <iostream>
 
-
 class vec3 {
-public:
+  public:
     vec3() {}
-    vec3(float e0, float el, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
-    inline float x() const { return e[0]; }
-    inline float y() const { return e[1]; }
-    inline float z() const { return e[2]; }
-    inline float r() const { return e[0]; }
-    inline float g() const { return e[1]; }
-    inline float b() const { return e[2]; }
-
-    inline const vec3& operator+() const ( return *this; }
-    inline vec3
-};
+    vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
+}
 
 
 
