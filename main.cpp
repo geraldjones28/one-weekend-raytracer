f1b2ae8b658c2a53d1d099c67b6657fce72c0427
GeraldJones <gajone12@asu.edu>
2025-04-21 18:18:31 -0700

    Completed the book and did the final exercise of creating the cover of the book! Sick. Still confused on a lot, but was great to get my feet wet. Depth of Field doesn't seem to be working too well but I'll get it going next time.

--- main.cpp
+++ main.cpp
@@ -26,6 +26,35 @@ vec3 color(const ray& r, hitable *world, int depth) {
     }
 }
 
+hitable *random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5,0.5,0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = drand48();
+            vec3 center(a+0.9*drand48(),0.2,b+0.9*drand48());
+            if ((center-vec3(4,0.2,0)).length() > 0.9) {
+                if (choose_mat < 0.8) { //diffuse
+                    list[i++] = new sphere(center,0.2, new lambertian(vec3(drand48()*drand48(),drand48()*drand48(),drand48()*drand48())));
+                } else if (choose_mat < 0.95) { //metal
+                    list[i++] = new sphere(center, 0.2,
+                        new metal(vec3(0.5*(1+drand48()), 0.5*(1+drand48()), 0.5*(1+drand48())), 0.5*drand48()));
+                } else { //glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new sphere(vec3(0,1,0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4,1,0), 1.0, new lambertian(vec3(0.4,0.2,0.1)));
+    list[i++] = new sphere(vec3(4,1,0), 1.0, new metal(vec3(0.7,0.6,0.5), 0.0));
+
+    return new hitable_list(list,i);
+}
+
 int main() {
     // Window size
     int nx = 800;
@@ -35,20 +64,22 @@ int main() {
 
     cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    hitable *list[5];
+    /*
+     *hitable *list[5];
     list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3))); // Middle Sphere
     list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0))); //Ground
     list[2] = new sphere(vec3(1, 0, -0.2), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3)); //Right Sphere
     list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5)); //Left Sphere
     list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5)); //Bubble effect in left sphere
-    hitable *world = new hitable_list(list,5);
+    */
+    hitable *world = random_scene();
 
-    vec3 lookfrom(0, 0.5, 2);
-    vec3 lookat(0, 0, -1);
-    float dist_to_focus = (lookfrom - vec3(0, 0, -1)).length();
-    float aperture = 5.0; // not working....
+    vec3 lookfrom(13, 2, 3);
+    vec3 lookat(0, 0, 0);
+    float dist_to_focus = 10.0;
+    float aperture = 1.0;
 
-    camera cam(lookfrom, lookat, vec3(0,1,0), 35, float(nx)/float(ny), aperture, dist_to_focus); //Viewpoint
+    camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx)/float(ny), aperture, dist_to_focus); //Viewpoint
 
     // Core Ray Tracing Loop
     for (int j = ny-1; j >= 0; j--) { // Top to Bottom
